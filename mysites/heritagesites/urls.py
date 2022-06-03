5df028cc1d212ba3f39c4148ed8d195de50b34d9
Munjugagit <alicemunjuga@gmail.com>
2022-06-03 23:50:30 +0300

    Number two commit

--- mysites/heritagesites/urls.py
+++ mysites/heritagesites/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from heritagesites.views import HomePageView
+from heritagesites.views import HomePageView, subcounty_datasets, historicalsite_points 
 
 urlpatterns = [
     path('index/', HomePageView.as_view(), name = 'home'),
+    path('subcounty_data/', subcounty_datasets, name = 'subcounty'),
+    path('historicalsite_data/', historicalsite_points, name = 'historicalsite'),
 ]
--- mysites/heritagesites/views.py
+++ mysites/heritagesites/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.core.serializers import serialize
+from django.http import HttpResponse
+from .models import Sub_counties
+from .models import Historicalsite
 
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = "index.html"
+
+def subcounty_datasets(request):
+    subcounty = serialize('geojson', Sub_counties.objects.all())
+    return HttpResponse(subcounty,content_type='json')
+
+def historicalsite_points(request):
+    historicalsite = serialize('geojson', Historicalsite.objects.all())
+    return HttpResponse(historicalsite,content_type='json')
